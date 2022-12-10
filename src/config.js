4fa73733a98fdf9f4f287d9a4ace053ef75a1af0
asouqi <asouqi@atypon.com>
2022-12-10 04:49:54 +0300

    performance fix

--- src/config.js
+++ src/config.js
@@ -1,4 +1,10 @@
 
 export const config = {
-    IS_DEVELOPMENT: !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+    IS_DEVELOPMENT: !process.env.NODE_ENV || process.env.NODE_ENV === 'development',
+    CACHE_OVERFLOW_PERCENTAGE: 90,
+    TEXLIVE_CACHE: 'texlive',
+    TEXLIVE_HEAD_CACHE: 'texlive-head',
+    WORKER_CACHE: 'worker',
+    WORKER_VERSION: 1,
+    TEXLIVE_VERSION: 1,
 }
--- src/service-worker.js
+++ src/service-worker.js
@@ -5,7 +5,8 @@ import { precacheAndRoute } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { Strategy } from 'workbox-strategies';
 import { createPartialResponse  } from 'workbox-range-requests';
-import { ExpirationPlugin } from 'workbox-expiration';
+import { CacheExpiration } from 'workbox-expiration';
+import {config} from "./config";
 
 clientsClaim();
 
@@ -14,6 +15,8 @@ precacheAndRoute(self.__WB_MANIFEST);
 class TexliveCacheStrategy extends Strategy {
   async _handle(request, handler) {
     const cache = await caches.open(this.cacheName);
+
+    await this.cleanCache();
 
     /**
      * will cache full data of range request to use it later for partial response
@@ -25,6 +28,7 @@ class TexliveCacheStrategy extends Strategy {
       } else {
         const response = await fetch(request.url);
         await cache.put(request.url, response.clone())
+        await this.updateCache(request.url);
         return await createPartialResponse(request, response);
       }
     } else {
@@ -32,27 +36,56 @@ class TexliveCacheStrategy extends Strategy {
 
       return cachedResponse || fetch(request).then((async fetchedResponse => {
         await cache.put(request.url, fetchedResponse.clone());
+        await this.updateCache(request.url);
         return fetchedResponse;
       }))
     }
   }
+
+  async updateCache(url){
+    const expirationManager = this.plugins[0]
+    if (expirationManager){
+      await expirationManager.updateTimestamp(url)
+    }
+  }
+
+  /**
+   * clear cache if we reach 90% of storage usage
+   * @return {Promise<void>}
+   */
+  async cleanCache(){
+    const expirationManager = this.plugins[0]
+    const estimate =  await navigator.storage.estimate()
+    const usedSize = parseFloat((estimate.usage / estimate.quota * 100).toFixed(2))
+    if (usedSize > config.CACHE_OVERFLOW_PERCENTAGE && expirationManager){
+      await expirationManager.expireEntries()
+    }
+  }
+
 }
 
 registerRoute(/(pdftex|bibtex|service)-worker.js/, new TexliveCacheStrategy({
-  cacheName: 'worker',
+  cacheName: config.WORKER_CACHE,
+  version: config.WORKER_VERSION,
 }))
 
 registerRoute(/(texlive\/.*)|texlive.lst/, new TexliveCacheStrategy({
-  cacheName: 'texlive',
+  cacheName: config.TEXLIVE_CACHE,
+  version: config.TEXLIVE_VERSION,
   plugins: [
-      new ExpirationPlugin({maxEntries: 2})
+    new CacheExpiration(config.TEXLIVE_CACHE, {
+      maxEntries: 128,
+    })
   ]
 }))
 
 registerRoute(/(texlive\/.*)|texlive.lst/, new TexliveCacheStrategy({
-  cacheName: 'texlive-head',
+  cacheName: config.TEXLIVE_HEAD_CACHE,
+  version: config.TEXLIVE_VERSION,
   plugins: [
-    new ExpirationPlugin({maxEntries: 2})
+    new CacheExpiration(config.TEXLIVE_HEAD_CACHE, {
+      maxEntries: 128,
+    })
   ]
 }), 'HEAD')
 
@@ -63,3 +96,11 @@ self.addEventListener('message', (event) => {
     self.skipWaiting();
   }
 });
+
+self.addEventListener('install', async (event) => {
+  await caches.delete(config.WORKER_CACHE)
+  await caches.delete(config.TEXLIVE_CACHE)
+  await caches.delete(config.TEXLIVE_HEAD_CACHE)
+  console.log("Clean up", config.WORKER_VERSION)
+  self.skipWaiting();
+})
